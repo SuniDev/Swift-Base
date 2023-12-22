5aef6091a6833ae8aa729a3c694038e943def319
SuniDev <sun-918@naver.com>
2023-12-22 15:20:25 +0900

    init project

---
+++ SunSwiftBaseProject/SunSwiftBaseProject/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SunSwiftBaseProject
+//
+//  Created by suni on 2023/12/12.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ SunSwiftBaseProject/SunSwiftBaseProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SunSwiftBaseProject
+//
+//  Created by suni on 2023/12/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
