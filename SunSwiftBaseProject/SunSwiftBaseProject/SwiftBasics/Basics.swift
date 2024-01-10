0846a16e5278781f561243c4bf0fa6399553f589
SuniDev <sun-918@naver.com>
2024-01-10 23:48:10 +0900

    Post : [iOS/Swift/Basic] 튜플 Tuple
    - https://sunidev.tistory.com/35

--- SunSwiftBaseProject/SunSwiftBaseProject/SwiftBasics/Basics.swift
+++ SunSwiftBaseProject/SunSwiftBaseProject/SwiftBasics/Basics.swift
@@ -24,4 +24,32 @@ class DataType {
         year = age
         print("year \(year) age \(age)")
     }
+    
+    // MARK: 튜플(Tuple)
+    func tupleTest() {
+        // String, Int, Double 타입을 갖는 튜플
+        var student: (name: String, grade: Int, score: Double) = ("suni", 2, 4.2)
+
+        // 요소 이름을 사용해 값 사용
+        print("이름: \(student.name), 학년: \(student.grade), 학점: \(student.score)")
+
+        // 요소 이름을 사용해 값 할당
+        student.grade = 3
+
+        // 인덱스를 통해 값 사용
+        print("이름: \(student.0), 학년: \(student.1), 학점: \(student.2)")
+        
+        // 인덱스를 통해 값 할당
+        student.2 = 4.3
+        
+        
+        // 튜플 별칭 지정
+        typealias StudentTuple = (name: String, grade: Int, score: Double)
+
+        let suni: StudentTuple = ("snui", 3, 4.3)
+        let mora: StudentTuple = ("mora", 1, 4.4)
+
+        print("이름: \(suni.name), 학년: \(suni.grade), 학점: \(suni.score)")
+        print("이름: \(mora.name), 학년: \(mora.grade), 학점: \(mora.score)")
+    }
 }
