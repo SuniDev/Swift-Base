a20f45b755d4c64cd132343ae40512a52957e2a6
SuniDev <sun-918@naver.com>
2024-01-23 22:50:25 +0900

    Basics 소스 정리

---
+++ SunSwiftBaseProject/SunSwiftBaseProject/SwiftBasics/CollectionType+Basics.swift
@@ -0,0 +1,157 @@
+//
+//  CollectionType+Basics.swift
+//  SunSwiftBaseProject
+//
+//  Created by suni on 1/23/24.
+//
+
+import Foundation
+
+// MARK: 배열(Array)
+class ArrayBasics {
+        
+    func countTest() {
+        let names: Array<String> = ["suni", "zico", "mino"]
+        
+        print(names.count)  // 3
+    }
+    
+    func emptyTest() {
+        let emptyArray: [Any] = [Any]()
+        
+        print(emptyArray.isEmpty)   // true
+    }
+    
+    static func firstLastTest() {
+        let names: Array<String> = ["suni", "zico", "mino", "zico"]
+        
+        print(names.firstIndex(of: "zico") ?? "index nil")  // 1
+        print(names.firstIndex(of: "pio") ?? "index nil")   // index nil
+        
+        print(names.first ?? "first nil")   // suni
+        print(names.last ?? "last nil")     // zico
+    }
+    
+    func appendTest() {
+        var names: Array<String> = ["suni", "zico", "mino"]
+        
+        names.append("alice")   // 마지막에 alice 추가
+        print(names)            // ["suni", "zico", "mino", "alice"]
+        
+        names.append(contentsOf: ["pio", "junny"]) // 맨 마지막에 pio, junny 추가
+        print(names)            // ["suni", "zico", "mino", "alice", "pio", "junny"]
+    }
+    
+    func insertTest() {
+        var names: Array<String> = ["suni", "zico", "mino"]
+        
+        names.insert("alice", at: 2)    // 인덱스 2에 삽입
+        print(names)                    // ["suni", "zico", "alice", "mino"]
+        
+        names.insert(contentsOf: ["pio", "junny"], at: 1)  // 인덱스 1의 위치에 pio, junny 삽입
+        print(names)                                        // ["suni", "pio", "junny", "zico", "alice", "mino"]
+    }
+    
+    func removeTest() {
+        var names: Array<String> = ["suni", "zico", "mino", "alice", "pio", "junny"]
+        
+        print(names.removeFirst())  // 첫번째 요소 제거 후 제거된 요소 반환 : suni
+        print(names)    // ["zico", "mino", "alice", "pio", "junny"]
+        
+        print(names.removeLast())   // 마지막 요소 제거 후 제거된 요소 반환 : junny
+        print(names)    // ["zico", "mino", "alice", "pio"]
+        
+        print(names.remove(at: 0))  // 인덱스 0번째 요소 제거 후 제거된 요소 반환 : zico
+        print(names)    // ["mino", "alice", "pio"]
+    }
+}
+
+// MARK: Dictionary(딕셔너리)
+class DictionaryBasics {
+    
+    // MARK: 딕셔너리 생성
+    // 키 - String / 값 - Int
+    var ageForName1: Dictionary<String, Int> = Dictionary<String, Int>()
+    var ageForName2: [String: Int] = [String: Int]()
+    var ageForName3: [String: Int] = [:]
+    var ageForName4: [String: Int] = ["suni": 25, "zico": 28, "mino": 27]
+    
+    // typealias를 사용하여 생성
+    typealias StringIntDictionary = [String: Int]
+    var ageForName5: StringIntDictionary = StringIntDictionary()
+    
+    // MARK: 기본 기능
+    static func basicTest() {
+        var ageForName: [String: Int] = ["suni": 25, "zico": 28, "mino": 27]
+        
+        print(ageForName.isEmpty)   // false
+        print(ageForName.count)     // 3
+        
+        // 키에 해당하는 값 반환
+        print(ageForName["suni"] ?? "suni is nil")   // 25
+        // 키에 해당하는 값이 없으면 기본값을 반환하도록 할 수 있다.
+        print(ageForName["pio", default: 0])   // 0
+        
+        // 값 수정
+        ageForName["zico"] = 30
+        print(ageForName["zico", default: 0])   // 30
+        
+        // 키:값 추가
+        ageForName["somin"] = 35   // somin라는 키로 35라는 값을 추가
+        print(ageForName["somin", default: 0])  // 35
+        
+        // 키에 해당하는 값을 삭제 후 삭제된 값 반환
+        print(ageForName.removeValue(forKey: "suni") ?? "suni is nil")  // 25
+        
+        // 위에서 해당 값이 이미 삭제되었으므로 nil 반환
+        print(ageForName.removeValue(forKey: "suni") ?? "suni is nil")  // suni is nil
+    }
+}
+
+// MARK: Set(세트)
+class SetBasics {
+    // MARK: 기본 기능
+    static func basicTest() {
+        var names1: Set<String> = Set<String>() // 빈 세트 생성
+        var names2: Set<String> = []            // 빈 세트 생성
+
+        // Array와 마찬가지로 대괄호 사용
+        var names: Set<String> = ["suni", "zico", "mino", "suni"]
+
+        // 그렇기 때문에 타입 추론을 사용하게 되면 컴파일러는 Array로 타입을 지정합니다.
+        var numbers = [100, 200, 300]
+        print(type(of: numbers))        // Array<Int>
+
+        print(names.isEmpty)     // false
+        print(names.count)       // 3 - 중복된 값은 허용되지 않아 suni는 1 개만 남음
+
+        names.insert("somin")
+        print(names.count)      // 4
+
+        print(names.remove("zico"))     // zico
+        print(names.remove("pio"))      // nil
+    }
+    
+    // MARK: 집합 연산 활용
+    static func setOperation() {
+        let aStudents: Set<String> = ["suni", "zico", "mino"]
+        let bStudents: Set<String> = ["suni", "pio", "june", "zico", "somin"]
+
+        // 교집합 ["suni", "zico"]
+        let intersectSet: Set<String> = aStudents.intersection(bStudents)
+        print(intersectSet)
+
+        // 여집합의 합(배타적 논리합) ["june", "mino", "pio", "somin"]
+        let symmetricDiffSet: Set<String> = aStudents.symmetricDifference(bStudents)
+        print(symmetricDiffSet)
+        
+        // 차집합 ["mino"]
+        let subtractSet: Set<String> = aStudents.subtracting(bStudents)
+        print(subtractSet)
+
+        // 합집합 ["somin", "zico", "suni", "mino", "june", "pio"]
+        let unionSet: Set<String> = aStudents.union(bStudents)
+        print(unionSet)
+        print(unionSet.sorted())    // ["june", "mino", "pio", "somin", "suni", "zico"]
+    }
+}
--- SunSwiftBaseProject/SunSwiftBaseProject/SwiftBasics/DataType+Basics.swift
+++ SunSwiftBaseProject/SunSwiftBaseProject/SwiftBasics/DataType+Basics.swift
@@ -1,13 +1,60 @@
 //
-//  String+Basics.swift
+//  DataType+Basics.swift
 //  SunSwiftBaseProject
 //
-//  Created by suni on 2024/01/05.
+//  Created by suni on 1/9/24.
 //
 
 import Foundation
 
-/// String 타입이 제공하는 기능(함수, 연산자, 프로퍼티 등) 테스트
+/// 데이터 타입 기본
+class DataType {
+    
+    // MARK: 타입 별칭
+    func typealiasTest() {
+        // 타입 별칭 선언
+        typealias MyInt = Int
+        typealias YourInt = Int
+        
+        // 타입 별칭 사용
+        let age: MyInt = 100
+        var year: YourInt = 2080
+        
+        // 같은 타입으로 취급
+        year = age
+        print("year \(year) age \(age)")
+    }
+    
+    // MARK: 튜플(Tuple)
+    func tupleTest() {
+        // String, Int, Double 타입을 갖는 튜플
+        var student: (name: String, grade: Int, score: Double) = ("suni", 2, 4.2)
+
+        // 요소 이름을 사용해 값 사용
+        print("이름: \(student.name), 학년: \(student.grade), 학점: \(student.score)")
+
+        // 요소 이름을 사용해 값 할당
+        student.grade = 3
+
+        // 인덱스를 통해 값 사용
+        print("이름: \(student.0), 학년: \(student.1), 학점: \(student.2)")
+        
+        // 인덱스를 통해 값 할당
+        student.2 = 4.3
+        
+        
+        // 튜플 별칭 지정
+        typealias StudentTuple = (name: String, grade: Int, score: Double)
+
+        let suni: StudentTuple = ("snui", 3, 4.3)
+        let mora: StudentTuple = ("mora", 1, 4.4)
+
+        print("이름: \(suni.name), 학년: \(suni.grade), 학점: \(suni.score)")
+        print("이름: \(mora.name), 학년: \(mora.grade), 학점: \(mora.score)")
+    }
+}
+
+// MARK: 문자열(String)
 class StringBasics {
     
     // MARK: 여러 줄의 문자열 """
